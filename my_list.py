e0ff28a21a615b8fc5f9e695f3b2ea1cb77ce370
dennismuuo044@gmail.com <dennismuuo044@gmail.com>
2024-08-26 08:43:54 +0300

    code for assignment 2

---
+++ my_list.py
@@ -0,0 +1,11 @@
+# an empty list called my_list
+my_list = []
+my_list.extend([10, 20, 30, 40])
+my_list.insert(1, 15)
+my_list.extend([50, 60, 70])
+my_list.pop()
+# Sort my_list in ascending order
+my_list.sort()
+# Find and print the index of the value 30 in my_list
+index = my_list.index(30)
+print('The index of the value 30 in my_list is:', index)
